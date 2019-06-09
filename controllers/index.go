874e52233bb4e132fb6eb735d31674dfbc69fec7
RockLD <lid18149@gmail.com>
2019-06-09 23:12:08 +0800

    nodes&index

--- controllers/index.go
+++ controllers/index.go
@@ -1,13 +1,18 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 type Index struct {
 
 }
 
 func (index Index)Index(c *gin.Context) {
-	
+	c.HTML(http.StatusOK,"index/index.html",gin.H{
+		"nodes":"",
+	})
 }
 
 func (index Index)First(c *gin.Context) {
